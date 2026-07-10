bc6a0fbea7074609f3c9d8cd2426bdc297767368
Pankaj Kevat <pankajkevat21@gmail.com>
2026-07-11 02:08:34 +0530

    Weather app application main interface

--- src/main/java/com/example/weatherapp/WeatherAppApplication.java
+++ src/main/java/com/example/weatherapp/WeatherAppApplication.java
@@ -1,6 +1,8 @@
 package com.example.weatherapp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class WeatherAppApplication {
     public static void main(String[] args) {
